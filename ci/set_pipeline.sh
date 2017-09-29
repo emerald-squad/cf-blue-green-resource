bba3d2ec001bf68d7385e0d039d3e25d86022383
Ukabu <emmanuel@pirsch.org>
2017-09-28 20:35:03 -0400

    change name of target

--- ci/set_pipeline.sh
+++ ci/set_pipeline.sh
@@ -2,7 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ALIAS=${1:-annuaire}
+ALIAS=${1:-emerald-squad}
 CREDENTIALS=${2:-$DIR/params-sample.yml}
 PIPELINE_NAME=${3:-cf-blue-green-resource}
 
