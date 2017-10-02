d66410981c789134878484d9acb94c254f2c18ac
Ukabu <emmanuel@pirsch.org>
2017-10-02 13:51:07 -0400

    now use vault to access secrets

--- ci/set_pipeline.sh
+++ ci/set_pipeline.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ALIAS=${1:-emerald-squad}
-CREDENTIALS=${2:-$DIR/params-sample.yml}
+CREDENTIALS=${2:-$DIR/params.sample.yml}
 PIPELINE_NAME=${3:-cf-blue-green-resource}
 
 echo y | fly -t "${ALIAS}" sp -p "${PIPELINE_NAME}" -c $DIR/pipeline.yml -l "${CREDENTIALS}"
